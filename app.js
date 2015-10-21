0b07bb081b016dab3a43191a9999b369792d5db5
perfidiousscript <sam.moss321@gmail.com>
2015-10-21 15:21:43 -0500

    Delete and CHange buttons

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var numContainers = 0;
 $(document).ready(function(){
 	$("body").on('click','button', function(){
 		numContainers++;
-		$("#holder").append("<div><span>"+ numContainers +"</span></div>");
+		$("#holder").append("<div><span>"+ numContainers +"</span><button class='delete'>Delete</button><button class='change'>Change</button></div>");
 	})
 });
